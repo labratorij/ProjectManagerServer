725bcdb6a4a3c1eebba6f10a09f7009a465a6842
enchar <deseq97@mail.ru>
2017-11-17 01:08:41 +0300

    Signed-off-by: enchar <deseq97@mail.ru>

--- src/main/java/com/labratorij/ProjectManager/Server/Application/controller/ProjectManagerController.java
+++ src/main/java/com/labratorij/ProjectManager/Server/Application/controller/ProjectManagerController.java
@@ -1,18 +1,31 @@
 package com.labratorij.ProjectManager.Server.Application.controller;
 
-import org.springframework.stereotype.Controller;
+import com.labratorij.ProjectManager.Server.Application.entity.Tasks;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.sql.Date;
+
+@RestController
 @RequestMapping("/test")
 public class ProjectManagerController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getRequset(ModelMap modelMap){
-        return "My request";
+    public Tasks getRequset(ModelMap modelMap){
+        return createMockRemind();
     }
+
+    private Tasks createMockRemind() {
+        Tasks tasks = new Tasks();
+        tasks.setId(1);
+        tasks.setTitle("Try");
+        tasks.setDate(new Date(2015,12,10));
+        return tasks;
+    }
+
+
 }
